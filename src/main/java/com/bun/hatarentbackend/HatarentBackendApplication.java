9b498053de804b5bd755ea510e70945dddda59ca
Tymofiy Bun <1931850@champlaincollege.qc.ca>
2021-12-08 20:08:28 -0500

    Initial commit

---
+++ src/main/java/com/bun/hatarentbackend/HatarentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bun.hatarentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatarentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatarentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bun/hatarentbackend/HatarentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bun.hatarentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HatarentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
