d76c2947ea40c8014eef41526f607b2b880f6bb6
Tymofiy Bun <1931850@champlaincollege.qc.ca>
2021-12-09 04:54:20 -0500

    Added methods to service

--- src/main/java/com/bun/hatarentbackend/property/businesslayer/PropertyService.java
+++ src/main/java/com/bun/hatarentbackend/property/businesslayer/PropertyService.java
@@ -1,6 +1,22 @@
 package com.bun.hatarentbackend.property.businesslayer;
 
+import com.bun.hatarentbackend.property.datalayer.PropertyDTO;
+import com.bun.hatarentbackend.property.datalayer.PropertyEntity;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface PropertyService
 {
+    public List<PropertyEntity> getAllProperties();
+    public PropertyEntity getPropertyByPropertyId(UUID propertyId);
+    public PropertyEntity updateProperty(PropertyEntity propertyEntity, PropertyEntity UpdatePropertyEntity);
+    public PropertyEntity createProperty(PropertyEntity propertyEntity);
+    public void deletePropertyByPropertyId(UUID propertyId);
 
+    public List<PropertyDTO> getAllPropertiesDTO();
+    public PropertyDTO getPropertyDTOByPropertyId(UUID propertyId);
+    public PropertyDTO updatePropertyDTO(UUID propertyId, PropertyEntity UpdatePropertyEntity);
+    public PropertyDTO createPropertyDTO(PropertyEntity propertyEntity);
+    public void deletePropertyDTOByPropertyId(UUID propertyId);
 }
