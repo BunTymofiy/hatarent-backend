35b308175bb4de49eb4ed452b6f6439ded14c350
timaboon <tymofiy@zelexio.com>
2022-01-18 00:38:11 -0500

    Login logout and register fully working

--- src/test/java/com/bun/hatarentbackend/user/UserRepoTest.java
+++ src/test/java/com/bun/hatarentbackend/user/UserRepoTest.java
@@ -1,4 +1,11 @@
 package com.bun.hatarentbackend.user;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class UserRepoTest {
+
 }
