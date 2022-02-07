a19984dac6b17b8a05e4eccccf2e5bf63bebb576
iana-sha <iana@zelexio.com>
2022-02-06 22:11:15 -0500

    Night & Repository tests fixed

--- src/test/java/com/bun/hatarentbackend/reservation/ReservationRepoTest.java
+++ src/test/java/com/bun/hatarentbackend/reservation/ReservationRepoTest.java
@@ -36,7 +36,7 @@ public class ReservationRepoTest {
     public void saveReservationTest() {
         Address address = new Address(uuidAddressId, "Street", "apartment","City", "State", "Zip", "Country", "Latitude", "Longitude");
         UUID uuid = UUID.randomUUID();
-        Property property1 = new Property(uuid, uuidHostId, address, 10, "SomeDescription", "Title","Name","Email",List.of("",""));
+        Property property1 = new Property(uuid, uuidHostId, address, 10, "SomeDescription", "Title","Name","Email",List.of("",""), 98.0, List.of(3.4,4.1));
         Date date1  = new Date(2021, 11, 17);
         Date date2  = new Date(2021, 11, 19);
         Reservation reservation1 = new Reservation(reservationId, property1, uuidHostId, date1, date2, 3, 150.0f);
@@ -50,7 +50,7 @@ public class ReservationRepoTest {
     {
         Address address = new Address(uuidAddressId, "Street", "apartment","City", "State", "Zip", "Country", "Latitude", "Longitude");
         UUID uuid = UUID.randomUUID();
-        Property property1 = new Property(uuid, uuidHostId, address, 10, "SomeDescription", "Title","Name","Email",List.of("",""));
+        Property property1 = new Property(uuid, uuidHostId, address, 10, "SomeDescription", "Title","Name","Email",List.of("",""), 98.0, List.of(3.4,4.1));
         Date date1  = new Date(2021, 11, 17);
         Date date2  = new Date(2021, 11, 19);
         Reservation reservation1 = new Reservation(reservationId, property1, uuidHostId, date1, date2, 3, 150.0f);
